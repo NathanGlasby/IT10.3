41adca4bdcc70518df3da9136384510ce6dec0e7
Nathan Glasby <nathan.glasby@gmail.com>
2026-07-29 10:21:31 +0200

    Add LoginScreen and RainyA coursework

---
+++ LoginScreen/src/loginscreen/LoginScreen.java
@@ -0,0 +1,24 @@
+package loginscreen;
+
+import javax.swing.JOptionPane;
+
+public class LoginScreen {
+
+    public static void main(String[] args) {
+        String userUserName = "Nate";
+        String userPassword = "ThisClass!";
+        
+        String userName = JOptionPane.showInputDialog("Enter your username.");
+        String password = JOptionPane.showInputDialog("Enter your password.");
+        
+        if (userName.equals(userUserName)) {
+            if (password.equals(userPassword)) {
+                System.out.println("Login successful.");
+            }
+        }
+        else {
+            System.out.println("Login failed.");
+        }
+    }
+    
+}
