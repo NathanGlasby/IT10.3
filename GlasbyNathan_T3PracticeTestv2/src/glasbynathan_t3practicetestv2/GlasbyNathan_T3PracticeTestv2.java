e115a580fb5ba55f484811950666b10948b09af6
Nathan Glasby <nathan.glasby@gmail.com>
2026-08-06 23:53:39 +0200

    Add Term 3 practice tests and if revision

---
+++ GlasbyNathan_T3PracticeTestv2/src/glasbynathan_t3practicetestv2/GlasbyNathan_T3PracticeTestv2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package glasbynathan_t3practicetestv2;
+
+/**
+ *
+ * @author natha
+ */
+public class GlasbyNathan_T3PracticeTestv2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+    
+}
